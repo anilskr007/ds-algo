971f42dd1d4df8f73c6529abd61caa9e5e3b8395
Birendra Rai <anilskr007@gmail.com>
2020-08-30 17:57:02 +0530

    inital setup

---
+++ Array/index.js
@@ -0,0 +1,15 @@
+let arr = [2,3,4,5,6]
+
+const sum = function(arr){
+    let sum =0;
+
+    for(let i=0; i< arr.length; i++){
+        
+        sum =(sum +arr[i]);
+    }
+
+
+    return sum
+    
+}
+console.log(arr[3])
